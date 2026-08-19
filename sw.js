d6be6ea6d2a336f57166119cf08101ad978e2fd7
sonalispot <30184696+sonalispot@users.noreply.github.com>
2026-08-18 20:39:40 -0400

    Update cache version to pppt-v4

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pppt-v3';
+const CACHE = 'pppt-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
                 './icon-192.png', './icon-512.png', './icon-180.png'];
 
